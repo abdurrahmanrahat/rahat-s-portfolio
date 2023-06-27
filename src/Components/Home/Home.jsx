754de4dc482bd4069cb436e657a1d61eafbf801e
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-06-27 06:55:47 +0600

    Portfolio project set-up

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            <Navbar></Navbar>
+
+            {/* Body Section */}
+            <h2>Home Page</h2>
+
+            {/* Footer */}
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
