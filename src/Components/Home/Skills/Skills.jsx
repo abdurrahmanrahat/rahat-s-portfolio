0a3ed68a905af21d60ee47567f0558ff204f9f34
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-09-19 16:19:35 +0600

    updated project list

--- src/Components/Home/Skills/Skills.jsx
+++ src/Components/Home/Skills/Skills.jsx
@@ -15,8 +15,8 @@ const Skills = () => {
                 <p className="md:w-3/5 mx-auto mt-2">Using a combination of state-of-the-art technologies and dependable open-source software, I develop user-focused, high-performing websites for mobile devices, tablets, and computers.</p>
                 <div className="grid grid-cols-3 md:grid-cols-6 my-6">
                     {
-                        skills.map(skill => <div key={skill._id} className="p-4">
-                            <img src={skill.image} className="w-40" alt="" />
+                        skills.map(skill => <div key={skill._id} className="p-4 flex flex-col justify-center items-center">
+                            <img src={skill.image} className="w-32" alt="" />
                             <h4>{skill.title}</h4>
                         </div>)
                     }
